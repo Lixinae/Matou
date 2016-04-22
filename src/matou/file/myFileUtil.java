017678e794ddba75cf67e86255cf30717b521c95
Vahen <naeos@laposte.net>
2016-04-23 00:29:20 +0200

    [UPDATE] myFileUtils Amelioration

--- src/matou/file/myFileUtil.java
+++ src/matou/file/myFileUtil.java
@@ -1,7 +1,6 @@
 package matou.file;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,12 +13,17 @@ import java.nio.file.Paths;
 public class myFileUtil {
 
     private final int MAX_SIZE = 1000; // TODO: 22/04/2016 -> change that
+    private final int BUFF_SIZE = 4096;
 
     public myFileUtil() {
 
     }
 
-
+    /**
+     * @param name nom du fichier en entrée
+     * @return bytebuffer à envoyé
+     * @throws IOException
+     */
     public static ByteBuffer readAndStoreInBuffer(String name) throws IOException {
         Path path = Paths.get(name);
         if (!Files.exists(path)) {
@@ -35,15 +39,55 @@ public class myFileUtil {
             System.out.println("Le fichier " + name + " est trop gros");
             return null;
         }
-        byte[] buffer = new byte[4096];
-        InputStream inputStream = Files.newInputStream(path);
-        int nbRead = 0;
+//        byte[] buffer = new byte[BUFF_SIZE];
+//        InputStream inputStream = Files.newInputStream(path);
+//        int nbRead = 0;
+//
+//
+//        ByteBuffer endBuffer = ByteBuffer.allocate(size.intValue());
+//        endBuffer.putInt(size.intValue());
+//        while ((inputStream.read(buffer)) != -1) {
+//            endBuffer.put(buffer);
+//        }
+//        return endBuffer;
+        byte[] buffer = Files.readAllBytes(path);
         ByteBuffer endBuffer = ByteBuffer.allocate(size.intValue());
-        endBuffer.putInt(size.intValue());
-        while ((nbRead = inputStream.read(buffer)) != -1) {
-            endBuffer.put(buffer);
-        }
+        endBuffer.put(buffer);
         return endBuffer;
+    }
+
+    /**
+     *
+     * @param byteBuffer buffer de reception des données
+     * @param filename nom du fichier voulu en sortie
+     * @return un boolean qui indique si on a bien tout écrit ou non
+     */
+    public static boolean readInBufferAndWriteInFile(ByteBuffer byteBuffer, String filename) {
+
+        byteBuffer.flip();
+//        byte[] buffer = new byte[BUFF_SIZE];
+
+        Path path = Paths.get(filename);
+        if (Files.exists(path)) {
+            System.out.println("Le fichier " + filename + " existe deja");
+            return false;
+        }
+        byte[] buff = byteBuffer.array();
+        try {
+            Files.write(path, buff);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        OutputStream outputStream = Files.newOutputStream(path);
+//        while (byteBuffer.hasRemaining()) {
+//
+//            byteBuffer.get(buffer);
+//
+//            outputStream.write(buffer);
+//        }
+
+
+        return true;
     }
 
 }
